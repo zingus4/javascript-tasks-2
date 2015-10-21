20f224a16d1051f55d1a7ee8fd1d7d33c1741c44
zingus4 <zingus4@gmail.com>
2015-10-21 13:59:51 +0500

    +

--- phoneBook.js
+++ phoneBook.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var phoneBook = new Array(); // Здесь вы храните записи как хотите
+var phoneBook = []; // Здесь вы храните записи как хотите
 
 /*
    Функция добавления записи в телефонную книгу.
@@ -8,40 +8,43 @@ var phoneBook = new Array(); // Здесь вы храните записи ка
 */
 module.exports.add = function add(name, phone, email) {
     if (isValidName(name) && isValidPhone(phone) && isValidEmail(email)){
-        phoneBook.push({'name':name,'phone':phone,'email':email, 'hash':name+";"+phone+";"+email});
-        console.log("Запись "+name+' '+phone+' '+email+" добавлена")
+        phoneBook.push({name:name, phone:phone, email:email, hash:name+';'+phone+';'+email});
+        console.log('Запись', name, phone, email, 'добавлена')
     }
-    console.log("Запись "+name+' '+phone+' '+email+" не корректна")
+    console.log('Запись', name, phone, email, 'не корректна')
 };
 
 function isValidEmail(email) {
-    if (/[^@;]+@[^@;]+\.\w/.test(email)) {
-        return true
-    }
-    return false
-}
-function isValidPhone(phone){
-    if (/^(([\+]?)?([\d]{0,3})|([\d]{1,2}))[\s]?(\(?([\d]{3}\))|[\d]{3})[\s|-]?[\d]{3}[\s|-]?[\d]{1}[\s|-]?[\d]{3}/.test(phone)) {
-        return true 
-    }
-    return false
-}
-function isValidName(name){
-  if (/\s/.test(name)) {
+    if (email==='') {
         return false
     }
-    return true  
+    return /[^@;]+@[^@;]+\.\w/.test(email);
+}
+function isValidPhone(phone){
+    if (phone==='') {
+        return false
+    }
+    return /^(\+?(\d{0,3})|(\d{1,2}))\s?(\(?(\d{3}\))|\d{3})[\s|-]?\d{3}[\s|-]?\d{1}[\s|-]?\d{3}/.test(phone);
+}
+function isValidName(name){
+    if (name==='') {
+        return false
+    }
+    return /\s/.test(name);
 }
 /*
    Функция поиска записи в телефонную книгу.
    Поиск ведется по всем полям.
 */
 module.exports.find = function find(query) {
-    console.log("Поиск записей по запросу: "+query)
-    var re = new RegExp(query);
+    if (';'.indexOf(query))){
+        console.log('Неправильный запрос для поиска!');
+        return 1;
+    }
+    console.log('Поиск записей по запросу:',query)
     for (var i = 0; i < phoneBook.length; i++) {
-        if (re.test(phoneBook[i]['hash'])) {
-            console.log(phoneBook[i]['name']+'  '+phoneBook[i]['phone']+'   '+phoneBook[i]['email'])
+        if (query.indexOf(hash))) {
+            console.log(phoneBook[i].name,phoneBook[i].phone,phoneBook[i].email)
         }
     }
     return 0;
@@ -53,8 +56,8 @@ module.exports.find = function find(query) {
 module.exports.remove = function remove(query) {
     var re = new RegExp(query);
     for (var i = 0; i < phoneBook.length; i++) {
-        if (re.test(phoneBook[i]['hash'])) {
-            console.log(phoneBook[i]['name']+'  '+phoneBook[i]['phone']+'   '+phoneBook[i]['email']+'   - запись удалена');
+        if (re.test(phoneBook[i].hash)) {
+            console.log(phoneBook[i].name,phoneBook[i].phone,phoneBook[i].email,'- запись удалена');
             delete phoneBook[i]
         }
     }
@@ -66,7 +69,7 @@ module.exports.remove = function remove(query) {
 */
 module.exports.importFromCsv = function importFromCsv(filename) {
     var data = require('fs').readFileSync(filename, 'utf-8');
-    console.log("Этот метод не хочет работать");
+    console.log('Этот метод не хочет работать');
     // Ваша чёрная магия:
     // - Разбираете записи и з `data`
     // - Добавляете каждую запись в книгу
@@ -77,7 +80,7 @@ module.exports.importFromCsv = function importFromCsv(filename) {
    Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
 */
 module.exports.showTable = function showTable() {
-    console.log("Этот метод не хочет работать");
+    console.log('Этот метод не хочет работать');
     // Ваша чёрная магия здесь
     return 0;
 };
